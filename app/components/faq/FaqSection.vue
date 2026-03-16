4cb4fdd5128a22c198f4c6dab962353f7b3628c2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-16 16:20:28 +0100

    :rocket: add faq section

---
+++ app/components/faq/FaqSection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { ComponentProps } from "~/types/generic/ui";
+import { cn } from "~/lib/utils";
+
+const props = defineProps<ComponentProps>();
+</script>
+
+<template>
+  <div :class="cn('grid gap-1.5', props.class)">
+    <h2 class="text-2xl font-bold text-center">
+      {{ $t("layout.faq.title") }}
+    </h2>
+
+    <UiAccordion collapsible>
+      <UiAccordionItem
+        v-for="item in 7"
+        :key="item"
+        :value="`question-${item}`"
+      >
+        <UiAccordionTrigger>
+          {{ $t(`layout.faq.questions.${item - 1}.question`) }}
+        </UiAccordionTrigger>
+        <UiAccordionContent>
+          <p class="whitespace-pre-wrap">
+            {{ $t(`layout.faq.questions.${item - 1}.answer`) }}
+          </p>
+        </UiAccordionContent>
+      </UiAccordionItem>
+    </UiAccordion>
+  </div>
+</template>
