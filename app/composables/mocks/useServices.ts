d2db5fdc432c2625b1bfea5162f0fb9af96fdee2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-17 09:29:49 +0100

    :truck: migrate services list to its own composable

---
+++ app/composables/mocks/useServices.ts
@@ -0,0 +1,32 @@
+import type { Listed } from "~/types/generic/object";
+import type { Service } from "~/types/entities/service";
+import { Aperture, Camera, Clapperboard, Globe, Laptop, Video } from "lucide-vue-next";
+
+export function useServices(): Listed<Service> {
+  return [
+    {
+      icon: Globe,
+      key: "web-site",
+    },
+    {
+      icon: Laptop,
+      key: "web-app",
+    },
+    {
+      icon: Camera,
+      key: "event-photo",
+    },
+    {
+      icon: Aperture,
+      key: "studio-photo",
+    },
+    {
+      icon: Video,
+      key: "event-video",
+    },
+    {
+      icon: Clapperboard,
+      key: "shooting-video",
+    },
+  ];
+}
---
+++ app/types/entities/service.ts
@@ -0,0 +1,6 @@
+import type { LucideIcon } from "lucide-vue-next";
+
+export interface Service {
+  icon: LucideIcon;
+  key: string;
+}
