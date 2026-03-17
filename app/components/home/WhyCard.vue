f618c3488d99a0ddb8d1675c4eedacea8b8e7052
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-17 21:18:18 +0100

    :rocket: add why card

---
+++ app/components/home/WhyCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { WhyCardProps } from "~/components/home/index";
+import { cn } from "~/lib/utils";
+
+const props = defineProps<WhyCardProps>();
+</script>
+
+<template>
+  <UiCard :class="cn('min-w-64 bg-card/20', props.class)">
+    <UiCardHeader class="flex flex-col">
+      <component :is="icon" />
+      <UiCardTitle>{{ title }}</UiCardTitle>
+      <UiCardDescription>{{ description }}</UiCardDescription>
+    </UiCardHeader>
+  </UiCard>
+</template>
