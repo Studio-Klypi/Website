4fff2f8b27c6f3f1817c4d53624fa20f51b569d6
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-20 10:03:51 +0100

    :rocket: setup ui configuration components

---
+++ app/components/interface/ThemeSwitcher.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Moon, Laptop, Sun } from "lucide-vue-next";
+
+const theme = useTheme();
+</script>
+
+<template>
+  <UiDropdownMenu>
+    <UiDropdownMenuTrigger as-child>
+      <UiButton
+        size="icon"
+        variant="ghost"
+      >
+        <ClientOnly>
+          <Moon v-if="theme === 'dark'" />
+          <Sun v-else-if="theme === 'light'" />
+          <template #fallback>
+            <Laptop />
+          </template>
+        </ClientOnly>
+      </UiButton>
+    </UiDropdownMenuTrigger>
+    <UiDropdownMenuContent>
+      <UiDropdownMenuRadioGroup v-model="$colorMode.preference">
+        <UiDropdownMenuRadioItem value="light">
+          Clair
+          <Sun class="ml-auto" />
+        </UiDropdownMenuRadioItem>
+        <UiDropdownMenuRadioItem value="dark">
+          Sombre
+          <Moon class="ml-auto" />
+        </UiDropdownMenuRadioItem>
+        <UiDropdownMenuRadioItem value="system">
+          Système
+          <Laptop class="ml-auto" />
+        </UiDropdownMenuRadioItem>
+      </UiDropdownMenuRadioGroup>
+    </UiDropdownMenuContent>
+  </UiDropdownMenu>
+</template>
