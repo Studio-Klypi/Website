35b9b752f04d653ba41a0dbbe2c2d33c2ef97369
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-20 10:04:37 +0100

    :rocket: setup topbar component in layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Layout from "~/components/composing/Layout.vue";
+import TopBar from "~/components/navigation/TopBar.vue";
 </script>
 
 <template>
   <Layout>
+    <TopBar />
+
     <NuxtPage />
   </Layout>
 </template>
