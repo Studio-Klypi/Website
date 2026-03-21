fbb7b7b15b0dabdc08c082f3037ae1e354a31669
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-21 10:32:39 +0100

    :rocket: restablish lucide-vue-next for branding icons

--- app/components/team/TeamMemberCard.vue
+++ app/components/team/TeamMemberCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Linkedin, Instagram } from "@lucide/vue";
+import { Linkedin, Instagram } from "lucide-vue-next";
 import type { TeamMemberCardProps } from "~/components/team/index";
 import { cn } from "~/lib/utils";
 
